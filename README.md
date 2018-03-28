d998501ea7546129f4225db1291f66dc4b99181c
eiffel <aif@auto-smart.com>
2018-03-28 10:56:06 +0800

    d_date dimention

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dw_dimention: for hive
 ## 1. d_date
 ```
-date_proxy_key  date_key1   date_key2    full_date    data_name   day_of_week  day_of_month   day_of_year  week_of_year week_en_name  week_cn_name  month_of_year  month_cn_name  month_en_name   year_name  quarter_of_year  quarter_cn_name  quarter_en_name  calendar_year_month  calendar_year_quarter
+date_proxy_key  date_key1   date_key2    full_date    date_name   day_of_week  day_of_month   day_of_year  week_of_year week_en_name  week_cn_name  month_of_year  month_cn_name  month_en_name   year_name  quarter_of_year  quarter_cn_name  quarter_en_name  calendar_year_month  calendar_year_quarter
 20000101        2000_01_01  2000-01-01   2000-01-01   2000-01-01       1             1              1            1        Saturday        星期六         1              一月         January          2000          1              第一季度          Q1               2017-01                 2017Q1
 ```
 ### 1.1. cd script and run:
@@ -12,14 +12,26 @@ date_proxy_key  date_key1   date_key2    full_date    data_name   day_of_week  d
 ### 1.3   create table:
 ```
 create table d_date(
-  date_proxy_key   String,
-  date_key1        String,
-  date_key2        String,
-  year             String,
-  month            String,
-  day              String,
-  quarter_name     String,
-  day_name_of_week String
+date_proxy_key          String comment '代理主键',
+date_key1               String comment '主键1',
+date_key2               String comment '主键2',
+full_date               Date   comment '日期的内容',
+data_name               String comment '日期的名字',
+day_of_week             Int    comment '每周第几天: 0~6',
+day_of_month            Int    comment '每月第几天: 0~30',
+day_of_year             Int    comment '每年第几天: 0~365',
+week_of_year            Int    comment '每年第几周: 0~53',
+week_en_name            String comment '周的英文名字',
+week_cn_name            String comment '周的中文名字',
+month_of_year           Int    comment '每年第几个月: 1~12',
+month_cn_name           String comment '月的中文名字',
+month_en_name           String comment '月的英文名字',
+year_name               String comment '年的名字',
+quarter_of_year         String comment '每年第一个季度: 1~4',
+quarter_cn_name         String comment '季度中文名字',
+quarter_en_name         String comment '季度英文名字',
+calendar_year_month     String comment '年-月标识',
+calendar_year_quarter   String comment '年季度标识'
 )
 row format delimited
 fields terminated by ' '
--- script/d_date.py
+++ script/d_date.py
@@ -2,8 +2,7 @@
 import os
 import datetime
 from datetime import timedelta
-# date_proxy_key     date_key1      date_key2           year      month   day      quarter_name    day_name_of_week
-# 20000101              2000_01_01     2000-01-01         2000       01           01             Q1                           Sunday
+
 start = '2000 01 01'
 end = '2030 12 31'
 flag = start
@@ -18,16 +17,78 @@ def get_date_key1(arg1):
 def get_date_key2(arg1):
     return arg1.strftime("%Y-%m-%d")
 
-def get_year(arg1):
+def  get_full_date(arg1):
+    return arg1.strftime("%Y-%m-%d")
+
+def  get_date_name(arg1):
+    return arg1.strftime("%Y-%m-%d")
+
+def get_day_of_week(arg1):
+    return arg1.strftime("%w")
+
+def get_day_of_month(arg1):
+    return bytes(int(arg1.strftime("%d")))
+
+def get_day_of_year(arg1):
+    return bytes(int(arg1.strftime("%j")))
+
+def get_week_of_year(arg1):
+    return bytes(int(arg1.strftime("%U")))
+
+def get_week_en_name(arg1):
+    return arg1.strftime("%A")
+
+def get_week_cn_name(arg1):
+    map = {'Monday': '星期一', 'Tuesday': '星期二', 'Wednesday': '星期三', 'Thursday': '星期四', 'Friday':  '星期五', 'Saturday': '星期六', 'Sunday':'星期日'}
+    return map[arg1.strftime("%A")]
+
+def get_month_of_year(arg1):
+    return bytes(int(arg1.strftime("%m")))
+
+def get_month_cn_name(arg1):
+    map = { 'January':'一月',
+                   'February': '二月',
+                   'March': '三月',
+                   'April': '四月',
+                    'May': '五月',
+                    'June': '六月',
+                    'July': '七月',
+                    'August': '八月',
+                     'September': '九月',
+                    'October': '十月',
+                    'November': '十一月',
+                    'December': '十二月'}
+    return map[arg1.strftime("%B")]
+
+def get_month_en_name(arg1):
+    return arg1.strftime("%B")
+
+def get_year_name(arg1):
     return arg1.strftime("%Y")
 
-def get_month(arg1):
-    return arg1.strftime("%m")
+def get_quarter_of_year(arg1):
+    month =arg1.strftime("%m")
+    if month in ['01', '02', '03']:
+    	return '1'
+    elif month in ['04', '05', '06']:
+    	return  '2'
+    elif month in ['07', '08', '09']:
+               return '3'
+    else:
+               return '4'
 
-def get_day(arg1):
-    return arg1.strftime("%d")
+def get_quarter_cn_name(arg1):
+    month =arg1.strftime("%m")
+    if month in ['01', '02', '03']:
+    	return '第一季度'
+    elif month in ['04', '05', '06']:
+    	return  '第二季度'
+    elif month in ['07', '08', '09']:
+               return '第三季度'
+    else:
+               return '第四季度'
 
-def get_quarter_name(arg1):
+def get_quarter_en_name(arg1):
     month =arg1.strftime("%m")
     if month in ['01', '02', '03']:
     	return 'Q1'
@@ -38,11 +99,36 @@ def get_quarter_name(arg1):
     else:
                return 'Q4'
 
-def get_day_name_of_week(arg1):
-    return arg1.strftime("%A")	
+def get_calendar_year_month(arg1):
+      return arg1.strftime("%Y-%m") 
+
+def get_calendar_year_quarter(arg1):
+      return arg1.strftime("%Y-") + get_quarter_en_name(arg1)
+
+# arg1 = datetime.datetime.strptime(flag, "%Y %m %d")
+# print get_date_proxy_key(arg1)
+# print get_date_key1(arg1)
+# print get_date_key2(arg1)
+# print get_full_date(arg1)
+# print get_date_name(arg1)
+# print get_day_of_week(arg1)
+# print get_day_of_month(arg1)
+# print get_day_of_year(arg1)
+# print get_week_of_year(arg1)
+# print get_week_en_name(arg1)
+# print get_week_cn_name(arg1)
+# print get_month_of_year(arg1)
+# print get_month_cn_name(arg1)
+# print get_month_en_name(arg1)
+# print get_year_name(arg1)
+# print get_quarter_of_year(arg1)
+# print get_quarter_cn_name(arg1)
+# print get_quarter_en_name(arg1)
+# print get_calendar_year_month(arg1)
+# print get_calendar_year_quarter(arg1)
 
 while flag != end:
     flag = datetime.datetime.strptime(flag, "%Y %m %d")
-    file.write(get_date_proxy_key (flag)+ ' ' + get_date_key1(flag) + ' ' + get_date_key2(flag) + ' '  + get_year(flag)  + ' ' + get_month(flag) + ' ' +  get_day(flag) + ' '  + get_quarter_name(flag) + ' ' + get_day_name_of_week(flag)  +"\n")
+    file.write(get_date_proxy_key(flag) + ' ' + get_date_key1(flag) + ' ' + get_date_key2(flag) + ' ' + get_full_date(flag) + ' ' + get_date_name(flag) + ' ' + get_day_of_week(flag) + ' ' + get_day_of_month(flag) + ' ' + get_day_of_year(flag) + ' ' + get_week_of_year(flag) + ' ' + get_week_en_name(flag) + ' ' + get_week_cn_name(flag) + ' ' + get_month_of_year(flag) + ' ' + get_month_cn_name(flag) + ' ' + get_month_en_name(flag) + ' ' + get_year_name(flag) + ' ' + get_quarter_of_year(flag) + ' ' + get_quarter_cn_name(flag) + ' ' + get_quarter_en_name(flag) + ' ' + get_calendar_year_month(flag) + ' ' + get_calendar_year_quarter(flag) +"\n")
     flag = (flag + timedelta(days = 1)).strftime("%Y %m %d")
 file.close()
